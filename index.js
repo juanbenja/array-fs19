632cecef809d645f9a7260e3a1d377510037a12e
JUAN GABRIEL ALMEIDA BENJAMIM <JUAN6105@digitalcollegesul.local>
2024-09-04 20:08:03 -0300

    Inicio do metodo com array

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1 - Criar uma lista vazia
+let listaJogos = ['Lost Planet', 'Crash 3', 'Ace Combat']
+
+let listaEstrelas = [{nome: 'Angelo', idade: 12}, 'betegeuse', 'sirius', 'anilam', 12]
+
+// 2- Alterar o último item para maiusculo
+
+//console.log(listaJogos[2].toUpperCase());
+
+// 3- Alterar todos os elementos para maiusculo
+let pessoas = [
+    'AnGelo lustosa',
+    'Juan bEnJamim',
+    'MAteuS ArANHA'
+]
+
+pessoas.map(item => console.log(item.toUpperCase()))
+//pessoas.map(function(item){return console.log(item.toUpperCase());})
+
